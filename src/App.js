14e864c54e5372fa78e921e14127c627e3b0cd18
Myrzamuratov <yeees.nooo02@gmail.com>
2023-06-19 17:10:06 +0400

    New branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <HomePage />
       <Footer />
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <div>hello</div>;
 };
 
 export default HomePage;
