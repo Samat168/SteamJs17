7873fab85872df52910c8c985b6034124bfd25d4
Myrzamuratov <yeees.nooo02@gmail.com>
2023-06-19 19:13:49 +0400

    Navigator

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import HomePage from "./pages/HomePage";
+import MainRoutes from "./Routes/MainRoutes";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <HomePage />
+      <MainRoutes />
       <Footer />
     </div>
   );
