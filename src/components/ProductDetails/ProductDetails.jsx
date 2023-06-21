551934560b5c68d208e212d7d355c8c72372de50
Samat168 <supersamat2004@gmail.com>
2023-06-21 18:52:27 +0600

    add

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,15 +1,8 @@
 import React, { useEffect } from "react";
 import { useProducts } from "../../contexts/ProductContextProvider";
 import { useParams } from "react-router-dom";
-import {
-  Box,
-  Button,
-  Card,
-  CardContent,
-  CardMedia,
-  Grid,
-  Typography,
-} from "@mui/material";
+import "./ProductDetails.css";
+import Navigate from "../Navigator/Navigate";
 
 const ProductDetails = () => {
   const { getProductDetails, productDetails } = useProducts();
@@ -20,90 +13,43 @@ const ProductDetails = () => {
     getProductDetails(id);
   }, []);
   return (
-    <Box sx={{ display: "flex", width: "75%", margin: "auto" }}>
-      <Typography
-        sx={{
-          width: "73%",
-          margin: "0 auto",
-          fontSize: "16px",
-          marginTop: "20px",
-        }}
-      >
-        Все игры / {productDetails?.category} / {productDetails?.title}
-      </Typography>
+    <div>
+      <Navigate />
+      <div className="container_details">
+        <div className="details_info">
+          <p className="detail-info">
+            Все игры / {productDetails?.category} / {productDetails?.title}{" "}
+          </p>
+          <p className="detail-title">{productDetails?.title}</p>
+        </div>
+        <div className="details_wrapper">
+          <div className="block_details_left">
+            <video width="600" height="370" controls autoPlay>
+              <source
+                src="https://cdn.akamai.steamstatic.com/steam/apps/256951745/movie480_vp9.webm?t=1686273391"
+                type="video/mp4"
+              />
+              Ваш браузер не поддерживает воспроизведение видео.
+            </video>
+            <div className="details_img">
+              <img className="img_product" src={productDetails?.pic1} alt="" />
+              <img className="img_product" src={productDetails?.pic2} alt="" />
+              <img className="img_product" src={productDetails?.pic3} alt="" />
+              <img className="img_product" src={productDetails?.pic4} alt="" />
+            </div>
+          </div>
 
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "20px",
-        }}
-      >
-        <Box>
-          <CardMedia
-            sx={{
-              height: 467,
-              width: 452,
-            }}
-            title="green iguana"
-          />
-          <Box
-            sx={{
-              display: "flex",
-              marginTop: "14px",
-              justifyContent: "space-around",
-              width: "70%",
-            }}
-          >
-            <CardMedia
-              sx={{
-                height: 60,
-                width: 60,
-              }}
-              image={productDetails?.pic1}
-              title="green iguana"
+          <div className="block_details_right">
+            <img
+              src={productDetails?.pic1}
+              alt=""
+              className="img_product_right"
             />
-            <CardMedia
-              sx={{
-                height: 60,
-                width: 60,
-              }}
-              image={productDetails?.pic2}
-              title="green iguana"
-            />
-            <CardMedia
-              sx={{
-                height: 60,
-                width: 60,
-              }}
-              image={productDetails?.pic3}
-              title="green iguana"
-            />
-          </Box>
-        </Box>
-
-        <Box sx={{ width: "33%" }}>
-          <CardContent>
-            <Typography
-              variant="h5"
-              component="h2"
-              gutterBottom
-              sx={{ color: "black", fontWeight: "bold", fontSize: "29px" }}
-            >
-              {productDetails?.title}
-            </Typography>
-            <Typography
-              variant="body1"
-              gutterBottom
-              sx={{ color: "black", fontWeight: "bold", fontSize: "23px" }}
-            >
-              {productDetails?.price} KGS
-            </Typography>
-          </CardContent>
-          <Box></Box>
-        </Box>
-      </Box>
-    </Box>
+            <p className="details_right_desc">{productDetails?.desc}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
