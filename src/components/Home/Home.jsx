ec856efc4dbcf8f2992ae6dd6f7dea649f82d298
Myrzamuratov <yeees.nooo02@gmail.com>
2023-06-22 16:40:22 +0400

    Nurkemel

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HomeCarousel from "./Carousel/HomeCarousel";
 import HomeCategory from "./Category/HomeCategory";
 import New from "./New/New";
 
+import { useState } from "react";
+import { useProducts } from "../../contexts/ProductContextProvider";
+
 const Home = () => {
+  const { products, getProducts } = useProducts();
+  useEffect(() => {
+    getProducts();
+  }, []);
+  const [action, setAction] = useState();
+
+  useEffect(() => {
+    products.forEach((product) => {
+      console.log(product);
+    });
+  }, [products]);
   return (
     <div>
-      <HomeCarousel />
+      <HomeCarousel action={action} />
       <HomeCategory />
       <New />
     </div>
