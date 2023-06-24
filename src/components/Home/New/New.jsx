2b2796fdf8b00246a2940f6ff67a126e5be63937
Myrzamuratov <yeees.nooo02@gmail.com>
2023-06-24 14:18:01 +0400

    category

--- src/components/Home/New/New.jsx
+++ src/components/Home/New/New.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./new.css";
 import WindowIcon from "@mui/icons-material/Window";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const New = ({ free, category, clearCat }) => {
   const [pict1, setPict1] = useState();
@@ -12,7 +13,7 @@ const New = ({ free, category, clearCat }) => {
     setPict2(img.pic2);
     setPict3(img.pic3);
   };
-
+  const navigate = useNavigate();
   return (
     <div
       style={{
@@ -108,12 +109,12 @@ const New = ({ free, category, clearCat }) => {
           {free.map((item) => (
             <div
               onMouseOver={(e) => showFoto(item)}
+              onClick={() => navigate(`/details/${item.id}`)}
               className="card_div"
               key={item.id}
               id={item.id}
               style={{
                 display: "flex",
-                margin: "0 0 10px",
               }}
             >
               <img
@@ -137,7 +138,7 @@ const New = ({ free, category, clearCat }) => {
                 {item.price === 0 ? (
                   <h3 className="games_text">Free</h3>
                 ) : (
-                  <h3 className="games_text">{item.price}</h3>
+                  <h3 className="games_text">{item.price} $</h3>
                 )}
               </div>
             </div>
