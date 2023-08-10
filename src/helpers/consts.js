efa2e115e6ca90681d7aed64904398bfb985275e
Samat168 <supersamat2004@gmail.com>
2023-08-11 01:36:51 +0600

    a

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -1,4 +1,4 @@
-export const API = "http://localhost:8000/products";
+export const API = "https://glistening-chaja-a2c32a.netlify.app/db.json";
 
 export const ACTIONS = {
   GET_PRODUCTS: "GET_PRODUCTS",
